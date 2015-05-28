702e75c391c46f232efe2a5824d535a3504b0aec
ciawalsh <cia.walsh@gmail.com>
2015-05-28 12:59:25 +0100

    changed server side os detection to client side screen width detection

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 var http = require('http');
 var path = require('path');
@@ -11,15 +11,9 @@ app.use('/js', express.static(path.join(__dirname, '/app/js')));
 app.set('port', (process.env.PORT || 3001))
 
 app.get('/', function(request, response) { 
-  // This is checking if the user is on a Mac or Windows.
-  if(process.platform === 'darwin' | 'win32'){
-    response.writeHead(302, {Location: 'http://www.candyspace.com'});
-    response.end();
-  } else {
-    response.render("index");
-   }
+  response.render("index");
 });
 
 http.createServer(app).listen(app.get('port'), function(request, response) {
   console.log("Node app is running at localhost:" + app.get('port'));
-}).listen('port');
+});
